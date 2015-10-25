4bf1f12f1d41a0347c4f77e180c3a98fa9aae5a0
Martin Abbott <the.abbot@webmonastery.com>
2015-10-25 21:55:22 +0800

    Updated to remove local

--- src/azure/configuration.lua
+++ src/azure/configuration.lua
@@ -2,16 +2,16 @@
 -- Martin Abbott
 -- 25/10/2015
 
-local serviceBusSas = "[Enter Service Bus Key]"
+serviceBusSas = "[Enter Service Bus Key]"
 serviceBusUrl = "[Enter Service Bus URL]"
-local eventHubSas = "[Enter Event Hub Key]"
-local eventHubUrl = "[Enter Event Hub URL]"
-local storageTableSas = "[Enter Storage Table Key]"
-local storageTableUrl = "[Enter Storage Table URL]"
-local storageBlobSas = "[Enter Storage Blob Key]"
-local storageBlobUrl = "[Enter Storage Blob URL]"
-local storageQueueSas = "[Enter Storage Queue Key]"
-local storageQueueUrl = "[Enter Storage Queue URL]"
-local documentDbKey = "[Enter DocumentDB Key]"
-local documentDbUrl = "[Enter DocumentDB URL]"
+eventHubSas = "[Enter Event Hub Key]"
+eventHubUrl = "[Enter Event Hub URL]"
+storageTableSas = "[Enter Storage Table Key]"
+storageTableUrl = "[Enter Storage Table URL]"
+storageBlobSas = "[Enter Storage Blob Key]"
+storageBlobUrl = "[Enter Storage Blob URL]"
+storageQueueSas = "[Enter Storage Queue Key]"
+storageQueueUrl = "[Enter Storage Queue URL]"
+documentDbKey = "[Enter DocumentDB Key]"
+documentDbUrl = "[Enter DocumentDB URL]"
 
