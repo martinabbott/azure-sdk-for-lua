63bbf404ce4da6198f3052f7d3c4aa0c79a4d9c0
Martin Abbott <the.abbot@webmonastery.com>
2015-10-27 17:53:40 +0800

    Updated main library files

--- src/callServiceBus.lua
+++ src/callServiceBus.lua
@@ -3,5 +3,20 @@
 -- 25/10/2015
 
 require("azure.configuration")
+local http = require("lib.socket.http")
 
-print(serviceBusUrl)
+--print(http)
+
+local headers = {
+			["User-Agent"] = "A custom user-agent!",
+			["Host"] = "Minecraft-Computer",
+			["api-key"] = "E1C721069E67E24A94EA73EFB75FA7AD",
+			["Content-Type"] = "application/json; charset=utf-8"
+        }
+
+
+
+
+local resp = http.get("http://www.example.com",headers)
+
+--print(resp)
